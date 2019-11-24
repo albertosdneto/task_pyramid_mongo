084d2ce0554e1e949911012eabf83ffe87ff37c8
albertosdneto <albertosdneto@gmail.com>
2019-11-24 12:52:44 -0300

    Part 04

--- task_manager/routes.py
+++ task_manager/routes.py
@@ -2,3 +2,5 @@ def includeme(config):
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
     config.add_route('tadd', '/add')
+    config.add_route('tedit', '/edit/{id}')
+    config.add_route('tdelete', '/delete/{id}')
--- task_manager/views.py
+++ task_manager/views.py
@@ -1,7 +1,9 @@
-from .forms import TaskForm
+from bson.objectid import ObjectId
 from pyramid.httpexceptions import HTTPFound
 from pyramid.url import route_url
 from pyramid.view import view_config
+
+from .forms import TaskForm, TaskUpdateForm
 
 
 @view_config(route_name='home', renderer='templates/home.jinja2')
@@ -11,6 +13,7 @@ def task_list(request):
         'tasks': tasks,
         'project': 'task_manager',
     }
+
 
 @view_config(route_name='tadd', renderer='templates/add.jinja2')
 def task_add(request):
@@ -22,3 +25,29 @@ def task_add(request):
         return HTTPFound(route_url('home', request))
 
     return {'form': form}
+
+
+@view_config(route_name='tedit', renderer='templates/edit.jinja2')
+def task_edit(request):
+
+    id = request.matchdict.get('id', None)
+    item = request.db['tasks'].find_one({'_id': ObjectId(id)})
+    form = TaskUpdateForm(request.POST,
+                          id=id, name=item['name'],
+                          active=item['active'])
+
+    if request.method == 'POST' and form.validate():
+        entry = form.data
+        entry['_id'] = ObjectId(entry.pop('id'))
+        request.db['tasks'].save(entry)
+        return HTTPFound(route_url('home', request))
+
+    return {'form': form}
+
+
+@view_config(route_name='tdelete')
+def task_delete(request):
+    id = request.matchdict.get('id', None)
+    if id:
+        request.db['tasks'].remove({'_id': ObjectId(id)})
+    return HTTPFound(route_url('home', request))
