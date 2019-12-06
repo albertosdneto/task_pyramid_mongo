0a45a1ad81cb4d4ce0f12c7084fb1af11038431c
Jackson Cunha <jackson.cereb@gmail.com>
2019-12-06 12:28:08 -0300

    Initial implementation of unit tests for file views.py.

---
+++ tests/test_views.py
@@ -0,0 +1,20 @@
+import pytest
+from pyramid import testing
+
+
+def test_task_list():
+    """ Unit tests for the function task_list. """
+    # Fake pyramid request, useful for testing.
+    request = testing.DummyRequest()
+
+    pytest.fail('Not implemented yet.')
+
+
+def test_task_add():
+    """ Unit tests for the function task_add. """
+    pytest.fail('Not implemented yet.')
+
+
+def test_task_add_invalid_form():
+    """ Unit tests for the function task_add when request contains invalid data. """
+    pytest.fail('Not implemented yet.')
